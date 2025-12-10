e9153e915dc73d7f0f32ac8f634a44ef2070a49b
blu <246948130+hvrryh-web@users.noreply.github.com>
2025-12-10 13:21:19 +1100

    Configure Vite React frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: path.resolve(__dirname),
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:8000'
-    }
+  build: {
+    outDir: 'dist'
   }
 });
